2e77693b9e8d6c73164e0960c9d9a95548153a98
Israel Calderon de la Cruz <icalderond@outlook.com>
2025-09-08 22:04:37 -0600

    + missing controller in App

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,16 @@ import dotenv from 'dotenv';
 import App from './app'
 import MeetingDayController from './controllers/meetingDay.controller';
 import StudentController from './controllers/student.controller';
+import UserController from './controllers/user.controller';
 import UserRollController from './controllers/userRoll.controller';
 
 dotenv.config();
 
 const app = new App(
     [
-        new StudentController(),
         new MeetingDayController(),
+        new StudentController(),
+        new UserController(),
         new UserRollController()
     ],
     process.env.PORT
