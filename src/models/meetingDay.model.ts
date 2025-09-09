a3286b401961b2e90bd9c20ee65d20f405baccc8
Israel Calderon de la Cruz <icalderond@outlook.com>
2025-09-08 22:18:27 -0600

    fix MeetingDay model

--- src/models/meetingDay.model.ts
+++ src/models/meetingDay.model.ts
@@ -2,13 +2,13 @@ import { DataTypes, Model, Optional } from 'sequelize';
 import sequelize from '../database';
 
 interface MeetingDayAttributes {
-    id:number;
+    id: number;
     day: Date;
     description: string;
     isActive: boolean;
 }
 
-interface MeetingDayCreationAttributes extends Optional<MeetingDayAttributes, 'day'> { }
+interface MeetingDayCreationAttributes extends Optional<MeetingDayAttributes, 'id'> { }
 
 class MeetingDay extends Model<MeetingDayAttributes, MeetingDayCreationAttributes> implements MeetingDayAttributes {
     public id!: number;
@@ -25,9 +25,8 @@ MeetingDay.init(
             primaryKey: true,
         },
         day: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
+            type: DataTypes.DATE,
+            allowNull: false,
         },
         description: {
             type: DataTypes.STRING,
