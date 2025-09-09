68384b112d3fdccd0be90c27e9b4f4df52b1ee5f
Israel Calderon de la Cruz <icalderond@outlook.com>
2025-09-08 21:10:14 -0600

    + sync database with sequelize

--- src/database.ts
+++ src/database.ts
@@ -9,12 +9,21 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST || "localhost",
         dialect: "postgres",
-        logging: false
-        // logging: console.log
+        // logging: false
+        logging: console.log
     })
 
 // sequelize.authenticate()
 //   .then(() => console.log('Connection has been established successfully.'))
 //   .catch((error) => console.error('Unable to connect to the database:', error));
+async function syncDatabase() {
+    try {
+        await sequelize.sync({ force: false }); // Or { alter: true } for altering existing tables
+        console.log('Database synchronized successfully.');
+    } catch (error) {
+        console.error('Error synchronizing database:', error);
+    }
+}
+syncDatabase();
 
 export default sequelize;
